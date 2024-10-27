559838d16d8c50e377f2ef9dedcadaabfe33133d
Nicola Spanu <nicspanu@gmail.com>
2024-10-27 14:40:45 +0100

    assertions_final

---
+++ tests/assertions_screen_shot.spec.ts
@@ -0,0 +1,8 @@
+import {test, expect} from '@playwright/test'
+
+test('Asserions screen-shot', async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    //Please take into consideration that the test is expected to fail the first time it runs since it has no screen-shot to compare to
+    await expect(page).toHaveScreenshot();
+    await page.close();
+})
