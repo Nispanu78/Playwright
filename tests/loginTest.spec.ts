b000ac884bfc623a76954acb6f85a626fadf0bcd
Nicola Spanu <nicspanu@gmail.com>
2024-10-27 12:05:54 +0100

    loginTest

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -5,4 +5,7 @@ test('Testing a login form', async({page})=>{
     await page.locator('input[placeholder="Username"]').fill('Admin');
     await page.locator('input[placeholder="Password"]').fill('admin123');
     await page.locator('(//button[normalize-space()="Login"])[1]').click();
+    await page.locator('//i[@class="oxd-icon bi-caret-down-fill oxd-userdropdown-icon"]').click();
+    await page.locator('text=Logout').click();
+    await page.close();
 })
